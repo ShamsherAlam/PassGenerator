9c9efe31d02711ef1527e6a5c1de2ad28a10c5bb
ShamsherAlam <same.ddps@gmail.com>
2020-04-25 16:13:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passgenerator';
+password : string = "";
+
+  onButtonClick(){
+    this.password = 'hello password';
+    console.log(this.password);
+
+  }
 }
