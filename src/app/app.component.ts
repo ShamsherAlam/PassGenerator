ee9e2cf51ec93a3c142e598d8e899c3abac1eac2
ShamsherAlam <same.ddps@gmail.com>
2020-04-25 23:31:17 +0530

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
+  useLetters = false;
+  userNumber = false;
+  useSymbol = false;
   password: string = "";
 
+  onChangeLength(value: string){
+    const parseValue = parseInt(value);
+    if(!isNaN(parseValue)){
+      this.length = parseValue;
+    }
+  }
+
+  onChangeUserLetter(){
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUserNumbers(){
+    this.userNumber = !this.userNumber;
+  }
+
+  onChangeUserSymbols(){
+    this.useSymbol = !this.useSymbol;
+  }
+
   onButtonClick() {
-    this.password = 'My Password';
+    // console.log(`
+    // User performing action on CheckBox
+    // letters : ${this.useLetters}
+    // number : ${this.userNumber}
+    // symbol : ${this.useSymbol}
+    // `);
+
+    const numbers = '1234567890';
+    const letters ='qwertyuioplkjhgfdsazxcvbnm';
+    const symbol = '!@#$%^&*()';  
+
+    let validChars = '';
+    if(this.useLetters){
+       validChars += letters;
+    }
+
+    if(this.userNumber){
+      validChars += numbers;
+   }
+
+   if(this.useSymbol){
+    validChars += symbol;
+ }
+
+  let genPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      genPassword += validChars[index];
+    }
+    this.password = genPassword;
   }
 
 }
